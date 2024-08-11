eb5a7d871b5c3fd4ccb6a1cb35dd0316fe6a3dc6
work97-admin <peter.yasser@inovaeg.com>
2024-08-11 11:03:21 +0300

    Add stock check

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -5,4 +5,26 @@ class Borrowing < ApplicationRecord
 
   enum status: { pending: 0, approved: 1, rejected: 2, returned: 3 }
   validates :number_of_days, presence: true
+
+  before_create :decrement_book_stock
+  after_update :increment_book_stock
+
+
+  def decrement_book_stock
+    raise 'This Book is not found. Choose another one or wait for other borrowers to return it' if book.stock <= 0
+    Book.transaction do
+      book.lock!
+      book.decrement!(:stock)
+    end
+  end 
+
+  def increment_book_stock
+    if self.status == 'returned' || self.status == 'rejected'
+    Book.transaction do
+      book.lock!
+      book.increment!(:stock)
+    end
+  end 
+
+
 end
